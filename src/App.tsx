eb36b2cc31027cde31e6e6c597cdb75a8ae1a14d
sivansuh <sivansuh.is@gmail.com>
2023-12-19 15:10:25 +0300

    tailwind webpack config added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>React js</div>;
+  return (
+    <div className="text-3xl font-bold underline text-red-400">React js</div>
+  );
 };
 
 export default App;
